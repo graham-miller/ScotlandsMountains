e19a62c6118e2d9b1594204fc15ba8fe6c20d1b0
Graham Miller <mail@grahammiller.net>
2022-02-20 20:24:40 +0000

    WIP

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LayoutModule } from './layout/layout.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ModalModule, BsModalService } from 'ngx-bootstrap/modal';
 import { Routes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { Routes } from './app.routes';
     LayoutModule,
     RouterModule.forRoot(Routes),
     BrowserAnimationsModule,
-    ModalModule
+    ModalModule,
+    HttpClientModule
   ],
   providers: [BsModalService],
   bootstrap: [AppComponent]
--- UI/src/app/layout/page1/page1.component.ts
+++ UI/src/app/layout/page1/page1.component.ts
@@ -1,15 +1,26 @@
-import { Component, TemplateRef } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { MountainsService } from 'src/app/services/mountains.service';
 
 @Component({
   selector: 'app-page1',
   templateUrl: './page1.component.html',
   styleUrls: ['./page1.component.css']
 })
-export class Page1Component {
+export class Page1Component implements OnInit {
   modalRef?: BsModalRef;
+  test: string = ''
+
+  constructor(
+    private modalService: BsModalService,
+    private mountains: MountainsService) {}
   
-  constructor(private modalService: BsModalService) {}
+  ngOnInit(): void {
+    this.mountains.getClassifications().subscribe(x => {
+      debugger;
+      this.test = x
+    });
+  }
   
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
