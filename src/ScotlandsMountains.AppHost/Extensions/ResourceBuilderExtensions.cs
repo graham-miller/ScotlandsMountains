430592783e5366a3adebe62e990421b5f648e3cf
Graham Miller <graham-miller@users.noreply.github.com>
2025-11-20 18:25:54 +0000

    WIP

--- src/ScotlandsMountains.AppHost/Extensions/ResourceBuilderExtensions.cs
+++ src/ScotlandsMountains.AppHost/Extensions/ResourceBuilderExtensions.cs
@@ -7,18 +7,18 @@ internal static class ResourceBuilderExtensions
     public static IResourceBuilder<ProjectResource> WithSwaggerUrls(this IResourceBuilder<ProjectResource> builder)
     {
         return builder.WithUrls(context =>
-         {
-             foreach (var url in context.Urls)
-             {
-                 if (string.IsNullOrEmpty(url.DisplayText))
-                 {
-                     url.Url += "/swagger";
-
-                     if (url?.Endpoint?.Scheme == "http")
-                         url.DisplayLocation = UrlDisplayLocation.DetailsOnly;
-                 }
-             }
-         });
+        {
+            context.Urls.ForEach(url =>
+            {
+                if (string.IsNullOrEmpty(url.DisplayText))
+                {
+                    url.Url += "/swagger";
+    
+                    if (url?.Endpoint?.Scheme == "http")
+                        url.DisplayLocation = UrlDisplayLocation.DetailsOnly;
+                    }
+            });
+        });
     }
 
     internal static IResourceBuilder<AzureStorageResource> RunAsEmulatorWithDefaultPorts(this IResourceBuilder<AzureStorageResource> builder)
@@ -33,4 +33,15 @@ internal static class ResourceBuilderExtensions
         return builder;
     }
 
+    public static IResourceBuilder<SqlServerServerResource> WithScotlandsMountainsDataBindMount(this IResourceBuilder<SqlServerServerResource> builder)
+    {
+        var path = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+            ".scotlandsmountains",
+            "mssql-data");
+
+        Directory.CreateDirectory(path);
+
+        return builder.WithDataBindMount(source: path);
+    }
 }
