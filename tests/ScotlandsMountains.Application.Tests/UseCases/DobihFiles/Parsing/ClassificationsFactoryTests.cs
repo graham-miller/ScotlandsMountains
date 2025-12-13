a20a74866cec1dd9875cab8ed65f70761111da7e
Graham Miller <graham-miller@users.noreply.github.com>
2025-12-13 06:23:31 +0000

    WIP

---
+++ tests/ScotlandsMountains.Application.Tests/UseCases/DobihFiles/Parsing/ClassificationsFactoryTests.cs
@@ -0,0 +1,32 @@
+﻿using ScotlandsMountains.Application.UseCases.DobihFiles.Parsing;
+
+namespace ScotlandsMountains.Application.Tests.UseCases.DobihFiles.Parsing;
+
+[TestFixture]
+public class ClassificationsFactoryTests
+{
+    [Test]
+    public void Build_ShouldReturnClassifications()
+    {
+        // arrange
+        var factory = new ClassificationsFactory();
+        
+        // act
+        var actual = factory.Build();
+
+        // assert
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(actual, Is.Not.Null);
+            Assert.That(actual, Is.Not.Empty);
+            Assert.That(actual.Count, Is.EqualTo(65));
+
+            var first = actual.First();
+            Assert.That(first.Name, Is.EqualTo("Munros"));
+            Assert.That(first.NameSingular, Is.EqualTo("Munro"));
+            Assert.That(first.DisplayOrder, Is.EqualTo(1));
+            Assert.That(first.Description, Does.StartWith("Scottish hills at least 3,000ft in height"));
+            Assert.That(first.DobihCode, Is.EqualTo("M"));
+        }
+    }
+}
